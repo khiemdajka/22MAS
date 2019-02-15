3c7e85e224b1ca374b5808f11e94a6d65722ceeb
Khiem Pham <khiem.dajka@gmail.com>
2019-02-15 11:01:11 -0500

    Update Week02_DataCleaning.sql

--- BANA7095-Graduate-Case-Study/Week02_DataCleaning.sql
+++ BANA7095-Graduate-Case-Study/Week02_DataCleaning.sql
@@ -1,6 +1,31 @@
 USE BANA7095
 GO
 
+--- 0. Create Ref tables
+CREATE TABLE Ref_Product (
+        Product VARCHAR(20),
+        Product_Type VARCHAR(20)
+        )
+GO
+
+INSERT INTO Ref_Product
+VALUES
+('BOAT', 'Watercraft'),
+('Collector Vehicle', 'Collector Vehicle'),
+('CV', 'Collector Vehicle'),
+('DWELLING FIRE', 'Site Built'),
+('EARTHQUAKE', 'Site Built'),
+('HOMEOWNER 4', 'Site Built'),
+('HOMEOWNERS', 'Site Built'),
+('JET SKI', 'Watercraft'),
+('Manufactured Housing', 'Manufactured Home'),
+('MOBILE HOME', 'Manufactured Home'),
+('MOTORCYCLE', 'Motorsports'),
+('Motorsports', 'Motorsports'),
+('Renters', 'Renters'),
+('Site Built Dwelling', 'Site Built'),
+('Watercraft', 'Watercraft')
+
 --- 1. Get clean data
 -- 1.1. Create new master table
 CREATE TABLE Isrn_Master (
@@ -36,6 +61,15 @@ SET Dist_Channel = RC. Dist_Channel
 FROM Isrn_Master IM
 JOIN Ref_Channel RC
         ON IM.Mkt_Channel = RC.Mkt_Channel
+
+ALTER TABLE Isrn_Master
+ADD Product_Type VARCHAR(20)
+
+UPDATE IM
+SET Product_Type = RP.Product_Type
+FROM Isrn_Master IM
+JOIN Ref_Product RP
+        ON IM.Product = RP.Product
 
 -- 1.4. Divide data into groups
 SELECT *
